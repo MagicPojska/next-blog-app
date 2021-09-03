37a57ee744123b02429f335f3787fe9f5eac0be4
Sam Sepiel <pojskicsafet@gmail.com>
2021-09-03 10:57:55 +0200

    Added routing structure

---
+++ pages/admin/[slug].js
@@ -0,0 +1,5 @@
+export default function AdminPostsEdit({}) {
+  return <main>
+      <h1>Edit post</h1>
+  </main>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div></div>;
+  return <div>
+    <Link prefetch={false} href={{
+      pathname= '/[username]',
+      query= { username: 'jeffd23'}
+    }}>
+      <a>Jeffs profile</a>
+    </Link>
+  </div>;
 }
